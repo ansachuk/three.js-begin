80a1abae77cf3918d03b75c04e04dd9e832354c8
Andrii Sachuk <ansachuk@icloud.com>
2025-01-24 13:53:45 +0100

    added many figures and textures

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import "./index.scss";
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	100,
+);
 
-camera.position.z = 5;
+camera.position.z = 6;
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -13,19 +18,53 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+cube.position.set(-3, 0, 0);
 
-const cube = new THREE.Mesh(geometry, material);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 8, 8);
+const sphereMaterial = new THREE.MeshPhongMaterial({
+	color: "teal",
+	emissive: "#fff",
+	shininess: 100,
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-scene.add(cube);
+sphere.position.set(3, 0, 0);
+
+const torus = new THREE.Mesh(
+	new THREE.TorusGeometry(0.7, 0.2, 10),
+	new THREE.MeshBasicMaterial({ color: "teal" }),
+);
+torus.position.set(0, 1.5, 1);
+
+const bgPlane = new THREE.Mesh(
+	new THREE.PlaneGeometry(window.innerWidth, window.innerHeight),
+	new THREE.MeshBasicMaterial({ color: "orange" }),
+);
+
+const paperTexture = new THREE.TextureLoader().load("src/assets/paper.jpg");
+
+const texturePlane = new THREE.Mesh(
+	new THREE.PlaneGeometry(2, 2),
+	new THREE.MeshBasicMaterial({ map: paperTexture }),
+);
+
+scene.add(bgPlane, texturePlane, torus, sphere, cube);
 
 function animate() {
 	requestAnimationFrame(animate);
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y -= 0.01;
+
+	sphere.rotation.x += 0.01;
+	sphere.rotation.y -= 0.01;
+
+	torus.rotation.x += 0.01;
+	torus.rotation.y -= 0.01;
 
 	renderer.render(scene, camera);
 }
